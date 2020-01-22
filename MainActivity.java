7d39a748f061ab4b2d25d28ab921f386043d871c
veeraragavanB <43211215+veeraragavanbe@users.noreply.github.com>
2020-01-22 06:38:03 +0530

    Code For Simple Qr Scanner. It Only Scan QrCodes.

--- MainActivity.java
+++ MainActivity.java
@@ -1,203 +1,73 @@
-package com.example.filupload;
+package com.example.qscaner;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.drm.DrmStore;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView im1;
+    EditText ed1;
+    Button generate;
 
-    Button select, upload;
-    TextView status;
+    String TAG="GenerateQrCode";
+    String inputvalue;
 
-    Uri pdfUri;
-    FirebaseAuth firebaseAuth;
-
-    FirebaseStorage storage;
-    FirebaseDatabase database;
-    ProgressDialog progressDialog;
-
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        storage = FirebaseStorage.getInstance();
-        database = FirebaseDatabase.getInstance();
-        //  StorageReference storage;
-        //  storage = FirebaseStorage.getInstance().getReference();
+        im1=findViewById(R.id.imgview);
+        ed1=findViewById(R.id.entext);
+        generate=findViewById(R.id.generate);
 
-        select = findViewById(R.id.recording);
-        upload = findViewById(R.id.upload);
-        status = findViewById(R.id.status);
-
-
-        select.setOnClickListener(new View.OnClickListener() {
+        generate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    selectpdf();
-                } else {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 9);
-                }
-            }
-        });
-
-
-        upload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (pdfUri != null)
-                    uploadFile(pdfUri);
-                else
-                    Toast.makeText(MainActivity.this, "Select File", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-    }
-
-    private void selectpdf() {
-        Intent in = new Intent();
-        //in.setType("application/pdf");
-        in.setType("audio/*");
-
-        in.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(in, 86);
-    }
-
-    private void uploadFile(Uri pdfUri) {
-
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        progressDialog.setTitle("Uploading File");
-        progressDialog.setProgress(0);
-        progressDialog.show();
-
-        final String fileName = System.currentTimeMillis() + "";
-        StorageReference storageReference = storage.getReference();
-        storageReference.child("Upload").child(fileName).putFile(pdfUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                String url = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString(); //important
-                DatabaseReference reference = database.getReference();
-                reference.child(fileName).setValue(url).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful())
-                            Toast.makeText(MainActivity.this, "File Succesfully Uploaded", Toast.LENGTH_SHORT).show();
-                        else
-                            Toast.makeText(MainActivity.this, "" , Toast.LENGTH_SHORT).show();
+                inputvalue = ed1.getText().toString().trim();
+                if (inputvalue.length()>0)
+                {
+                    WindowManager manager=(WindowManager)getSystemService(WINDOW_SERVICE);
+                    Display display = manager.getDefaultDisplay();
+                    Point point=new Point();
+                    display.getSize(point);
+                    int width=point.x;
+                    int height=point.y;
+                    int smallerdimension=width < height ? width:height;
+                    smallerdimension=smallerdimension*3/4;
+                    qrgEncoder=new QRGEncoder(inputvalue,null, QRGContents.Type.TEXT,smallerdimension);
+                    try {
+                        bitmap=qrgEncoder.encodeAsBitmap();
+                        im1.setImageBitmap(bitmap);
+                    }
+                    catch (WriterException e)
+                    {
+                        Log.v(TAG,e.toString());
 
                     }
-                });
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MainActivity.this, "File Not Succesfully Uploaded", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                int currentProgress = (int) (100 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
-                progressDialog.setProgress(currentProgress);
+
+                }
+                else
+                    {
+                        ed1.setError("Need Text");
+                    }
             }
         });
-
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if (requestCode == 9 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            selectpdf();
-        } else {
-            Toast.makeText(MainActivity.this, "Please Provide permission", Toast.LENGTH_SHORT).show();
-
-        }
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 86 && resultCode == RESULT_OK && data != null) {
-            pdfUri = data.getData();
-            status.setText("A file is selected" + data.getData().getLastPathSegment());
-        } else {
-            Toast.makeText(MainActivity.this, "Select the Pdf", Toast.LENGTH_SHORT).show();
-        }
     }
 }
-
-
-/*
-        upload.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View v) {
-
-        Intent in=new Intent();
-        in.setType("application/pdf");
-        in.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(in,1);
-        }
-
-        });
-        }
-
-@Override
-protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK)
-        {
-        if(requestCode==1)
-        {
-        Uri uri = null;
-        if (data!=null)
-        {
-        uri=data.getData();
-        }
-        if (uri!=null)
-        {
-        String[] filepath={"applicaiton/pdf"};
-        Cursor cursor=getContentResolver().query(uri,filepath,null,null,null);
-        }
-        }
-        }
-        }*/
